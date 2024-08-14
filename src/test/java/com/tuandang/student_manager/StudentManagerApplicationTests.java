7d023d3fca53641ce0c2e069ff11ce5587e6e163
million-dollar-dev <hoangtuan1304@hotmail.com>
2024-08-14 17:24:18 +0700

    initial

---
+++ src/test/java/com/tuandang/student_manager/StudentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuandang.student_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
