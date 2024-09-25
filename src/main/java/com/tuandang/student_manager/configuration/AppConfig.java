056cc687e989d6d7e96f6136b18f554c8f246f80
million-dollar-dev <hoangtuan1304@hotmail.com>
2024-09-25 22:27:47 +0700

    spring security

---
+++ src/main/java/com/tuandang/student_manager/configuration/AppConfig.java
@@ -0,0 +1,24 @@
+package com.tuandang.student_manager.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public WebMvcConfigurer corsConfiguration () {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("**")
+                        .allowedOrigins("http://localhost:")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
