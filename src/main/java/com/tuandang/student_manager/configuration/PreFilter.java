0ae546fcd78948c41abc3a4b9e8e09d39d4ba3ab
million-dollar-dev <hoangtuan1304@hotmail.com>
2024-10-09 20:48:27 +0700

    security access token

--- src/main/java/com/tuandang/student_manager/configuration/PreFilter.java
+++ src/main/java/com/tuandang/student_manager/configuration/PreFilter.java
@@ -1,10 +1,22 @@
 package com.tuandang.student_manager.configuration;
 
+import com.tuandang.student_manager.service.IJwtService;
+import com.tuandang.student_manager.service.IUserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,10 +24,37 @@ import java.io.IOException;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class PreFilter extends OncePerRequestFilter {
+    IJwtService jwtService;
+    IUserService userService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         log.info("-------------PreFilter------------");
+        filterChain.doFilter(request, response);
+
+        final String authorization = request.getHeader("Authorization");
+
+        if (StringUtils.isBlank(authorization) || !authorization.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final String token = authorization.substring("Bearer ".length());
+        final String username = jwtService.extractUsername(token);
+
+        if (StringUtils.isNotEmpty(username) && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userService.userDetailsService().loadUserByUsername(username);
+            if (jwtService.isValid(token, userDetails)) {
+                SecurityContext context = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                context.setAuthentication(authentication);
+                SecurityContextHolder.setContext(context);
+            }
+        }
+
         filterChain.doFilter(request, response);
     }
 }
